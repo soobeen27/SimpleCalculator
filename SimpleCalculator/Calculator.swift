8803d60f9a4f41e0041fa43e2fa0e46ce765860b
soobeen27 <youdew27@gmail.com>
2024-06-04 21:48:59 +0900

    finish lv3

--- SimpleCalculator/Calculator.swift
+++ SimpleCalculator/Calculator.swift
@@ -7,22 +7,74 @@
 
 import Foundation
 
+
 class Calculator {
-    func calulate(firstNum: Int, oper: String, secondNum: Int) -> Int {
-        switch oper {
+    let add = AddOperation()
+    let substract = SubstractOperation()
+    let multiple = MultiplyOperation()
+    let divide = DivideOperation()
+    
+    func getInfo(from expression: String) -> infoCalc{
+        var numArr: [Int] = []
+        let oper = ["+","-","*","/","%"]
+        
+        for sep in oper {
+            if expression.contains(sep) {
+                numArr = expression.components(separatedBy: sep).map
+                {(temp: String) -> Int in
+                    guard let num = Int(temp) else { return 0 }
+                    return num
+                }
+                return infoCalc(num: numArr, oper: sep)
+            }
+        }
+        return infoCalc(num: [0,0], oper: "Error")
+    }
+    
+    
+    func calc(info: infoCalc) -> Int {
+        switch info.oper {
         case "+":
-            return firstNum + secondNum
+            return add.add(n: info.num[0], n2: info.num[1])
         case "-":
-            return firstNum - secondNum
+            return substract.substract(n: info.num[0], n2: info.num[1])
         case "*":
-            return firstNum * secondNum
+            return multiple.multiple(n: info.num[0], n2: info.num[1])
         case "/":
-            return firstNum / secondNum
+            return divide.divide(n: info.num[0], n2: info.num[1])
         case "%":
-            return firstNum % secondNum
+            return divide.remain(n: info.num[0], n2: info.num[1])
         default:
             print("+ - * / 중에 입력해주세요")
             return 0
         }
     }
+}
+
+class AddOperation {
+    func add(n: Int, n2: Int) -> Int{
+        return n + n2
+    }
+}
+class SubstractOperation {
+    func substract(n: Int, n2: Int) -> Int{
+        return n - n2
+    }
+}
+class MultiplyOperation {
+    func multiple(n: Int, n2: Int) -> Int{
+        return n * n2
+    }
+}
+class DivideOperation {
+    func divide(n: Int, n2: Int) -> Int{
+        return n / n2
+    }
+    func remain(n: Int, n2: Int) -> Int {
+        return n % n2
+    }
+}
+struct infoCalc {
+    let num: [Int]
+    let oper: String
 }
--- SimpleCalculator/main.swift
+++ SimpleCalculator/main.swift
@@ -9,54 +9,35 @@ import Foundation
 
 print(
 """
-***************************
-*                         *
-*      Calculator Lv.2    *
-*                         *
-***************************
+*********************************************
+*                                           *
+*                Calculator Lv.3            *
+*                                           *
+*       띄어쓰기 없이 식을 넣어주세요 ex) 3+5       *
+*   프로그램을 종료하고 싶으시다면 stop을 입력해주세요   *
+*                                           *
+*********************************************
 """
 )
 
 let calculator = Calculator()
-
-calc()
-//func calc() {
-//    print("첫번째 숫자를 입력해주세요:")
-//    guard let firstNum = readLine() else { return }
-//    print("수식을 입력해주세요 ex(+,-,*,/,%):")
-//    guard let oper = readLine() else { return }
-//    print("두번째 숫자를 입력해주세요:")
-//    guard let secondNum = readLine() else { return }
-//    let sum = calculator.calulate(firstNum: Int(firstNum)!, oper: oper, secondNum: Int(secondNum)!)
-//    print("정답은:\(sum)")
-//}
-
-func calc() {
-    while true {
-        print("첫번째 숫자를 입력해주세요:", terminator: "")
-        guard let firstLine = readLine() else { return }
-        guard let firstNum = Int(firstLine) else
-        {
-            print("다시 입력해주세요")
-            continue
-        }
-        print("수식을 입력해주세요 ex(+,-,*,/,%):", terminator: "")
-        guard let seconLine = readLine() else { return }
-        if seconLine != "+" && seconLine != "-" && seconLine != "*" && seconLine != "/" && seconLine != "%" {
-            print("다시 입력해주세요")
-            continue
-        }
-        print("두번째 숫자를 입력해주세요:", terminator: "")
-        guard let thirdLine = readLine() else { return }
-        guard let thirdNum = Int(thirdLine) else
-        {
-            print("다시 입력해주세요")
-            continue
-        }
-        let sum = calculator.calulate(firstNum: firstNum, oper: seconLine, secondNum: thirdNum)
-        print("정답은:\(sum)")
-        return
+while true {
+    print("입력: ",terminator: "")
+    let input = readLine()
+    guard let input = input else 
+    {
+        print("다시 입력해주세요!")
+        continue
     }
+    if input == "stop" {
+        break
+    }
+    let info = calculator.getInfo(from: input)
+    if info.num.count != 2 {
+        print("다시 입력해주세요!")
+        continue
+    }
+    print(calculator.calc(info: info))
 }
 
 
